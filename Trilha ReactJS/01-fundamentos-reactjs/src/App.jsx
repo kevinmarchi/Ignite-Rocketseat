5e2ce4c5bbd05706a8001b07d8cfc9c2a8188595
Kevin Marchi <53917192+kevinmarchi@users.noreply.github.com>
2023-01-19 22:26:03 -0300

    01-fundamentos-reactjs - Estrutura da Aplicação

--- Trilha ReactJS/01-fundamentos-reactjs/src/App.jsx
+++ Trilha ReactJS/01-fundamentos-reactjs/src/App.jsx
@@ -1,16 +1,29 @@
+import { Header } from "./components/Header"
 import { Post } from "./Post"
+import { Sidebar } from "./components/Sidebar"
+
+import styles from "./App.module.css"
+
+import './global.css'
 
 export function App() {
   return (
     <div>
-      <Post 
-        author="Kevin Marchi" 
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat ducimus veritatis adipisci repudiandae illo ullam quo maxime voluptatum, saepe iste sit autem sequi at? Cum in ratione iusto vero at." 
-      />
-      <Post 
-        author="Autor Desconhecido" 
-        content="Post Desconhecido" 
-      />
+      <Header />
+      
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+        <Post 
+          author="Kevin Marchi" 
+          content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat ducimus veritatis adipisci repudiandae illo ullam quo maxime voluptatum, saepe iste sit autem sequi at? Cum in ratione iusto vero at." 
+        />
+        <Post 
+          author="Autor Desconhecido" 
+          content="Post Desconhecido" 
+        />
+        </main>
+      </div>
     </div>
   )
 }

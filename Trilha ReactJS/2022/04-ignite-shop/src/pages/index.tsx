754540cba59f855eb0bce1498f9a08ad1fdd9719
Kevin Marchi <53917192+kevinmarchi@users.noreply.github.com>
2023-02-09 16:59:44 -0300

    04-ignite-shop - Estrutura visual

--- Trilha ReactJS/2022/04-ignite-shop/src/pages/index.tsx
+++ Trilha ReactJS/2022/04-ignite-shop/src/pages/index.tsx
@@ -1,26 +1,54 @@
-import { styled } from "../styles"
+import { HomeContainer, Product } from "@/styles/pages/home";
+import Image from "next/image";
+import { useKeenSlider } from 'keen-slider/react'
 
+import camiseta1 from "@/assets/camisetas/1.png"
+import camiseta2 from "@/assets/camisetas/2.png"
+import camiseta3 from "@/assets/camisetas/3.png"
 
-const Button = styled('button', {
-  backgroundColor: "$rocketseat",
-  borderRadius: 4,
-  border: 0,
-  padding: '4px 8px',
-
-  span: {
-    fontWeight: 'bold',
-  },
-
-  '&:hover': {
-    filter: 'brightness(0.8)',
-  }
-})
+import 'keen-slider/keen-slider.min.css'
 
 export default function Home() {
-  return (
-    <Button>
-      <span>Teste</span>
-      Enviar
-    </Button>
-  )
+	const [sliderRef] = useKeenSlider({
+		slides: {
+			perView: 3,
+			spacing: 48,
+		}
+	})
+
+  	return (
+    	<HomeContainer ref={sliderRef} className="keen-slider">
+			<Product className="keen-slider__slide">
+				<Image src={camiseta1} alt="" width={520} height={480} />
+				<footer>
+					<strong>Camiseta X</strong>
+					<span>R$ 79,90</span>
+				</footer>
+			</Product>
+
+			<Product className="keen-slider__slide">
+				<Image src={camiseta2} alt="" width={520} height={480} />
+				<footer>
+					<strong>Camiseta Y</strong>
+					<span>R$ 79,90</span>
+				</footer>
+			</Product>
+
+			<Product className="keen-slider__slide">
+				<Image src={camiseta3} alt="" width={520} height={480} />
+				<footer>
+					<strong>Camiseta Z</strong>
+					<span>R$ 79,90</span>
+				</footer>
+			</Product>
+
+			<Product className="keen-slider__slide">
+				<Image src={camiseta3} alt="" width={520} height={480} />
+				<footer>
+					<strong>Camiseta Z</strong>
+					<span>R$ 79,90</span>
+				</footer>
+			</Product>
+    	</HomeContainer>
+  	)
 }

e0bdb9a562346d53fc373a78da7468b4397ce9a6
Kevin Marchi <53917192+kevinmarchi@users.noreply.github.com>
2023-03-23 16:56:37 -0300

    08-coffee-delivery - Creating cards

--- Trilha ReactJS/2022/08-coffee-delivery/src/pages/Home/index.tsx
+++ Trilha ReactJS/2022/08-coffee-delivery/src/pages/Home/index.tsx
@@ -1,47 +1,100 @@
-import { Container, ContainerImage, ContentContainer, ImageContainer, VantageIconBox, VantageItem, VantageItems } from "./styles";
+import { Card, CardContent, CardFooter, CardHeader, CatalogContainer, Container, ContainerImage, ContentContainer, IncreaseInput, VantageIconBox, VantageItem, VantageItems } from "./styles";
 import coffeeMainImg from '../../assets/coffee-main.png'
-import { Coffee, Package, ShoppingCart, Timer } from "phosphor-react";
+import { Coffee, Minus, Package, Plus, ShoppingCart, Timer } from "phosphor-react";
+import coffeeTradicional from '../../assets/coffee-expresso.png'
+import { Controller, useForm } from "react-hook-form";
 
 export function Home() {
+
+    const {register, handleSubmit, formState: {errors}, reset, control} = useForm({
+        defaultValues: {
+            itemAmount: 1
+        }
+    })
+
+    function handleAddItensCard(data: any) {
+        console.log(data)
+    }
+
     return (
-        <ContainerImage>
-            <Container>
-                <ContentContainer>
-                    <h2>Encontre o café perfeito para qualquer hora do dia</h2>
-                    <h4>Com o Coffee Delivery você recebe seu café onde estiver, a qualquer hora</h4>
+        <>
+            <ContainerImage>
+                <Container>
+                    <ContentContainer>
+                        <h1>Encontre o café perfeito para qualquer hora do dia</h1>
+                        <h4>Com o Coffee Delivery você recebe seu café onde estiver, a qualquer hora</h4>
 
-                    <VantageItems>
-                        <VantageItem>
-                            <VantageIconBox bgcolor={"yellow-dark"}>
-                                <ShoppingCart weight="fill" />
-                            </VantageIconBox>
-                            <p>Compra simples e segura</p>
-                        </VantageItem>
-                        <VantageItem>
-                            <VantageIconBox bgcolor={"base-text"}>
-                                <Timer weight="fill" />
-                            </VantageIconBox>
-                            <p>Embalagem mantém o café intacto</p>
-                        </VantageItem>
-                        <VantageItem>
-                            <VantageIconBox bgcolor={"yellow"}>
-                                <Package weight="fill" />
-                            </VantageIconBox>
-                            <p>Entrega rápida e rastreada</p>
-                        </VantageItem>
-                        <VantageItem>
-                            <VantageIconBox bgcolor={"purple"}>
-                                <Coffee weight="fill" />
-                            </VantageIconBox>
-                            <p>O café chega fresquinho até você</p>
-                        </VantageItem>
-                    </VantageItems>
-                </ContentContainer>
+                        <VantageItems>
+                            <VantageItem>
+                                <VantageIconBox bgcolor={"yellow-dark"}>
+                                    <ShoppingCart weight="fill" />
+                                </VantageIconBox>
+                                <p>Compra simples e segura</p>
+                            </VantageItem>
+                            <VantageItem>
+                                <VantageIconBox bgcolor={"base-text"}>
+                                    <Timer weight="fill" />
+                                </VantageIconBox>
+                                <p>Embalagem mantém o café intacto</p>
+                            </VantageItem>
+                            <VantageItem>
+                                <VantageIconBox bgcolor={"yellow"}>
+                                    <Package weight="fill" />
+                                </VantageIconBox>
+                                <p>Entrega rápida e rastreada</p>
+                            </VantageItem>
+                            <VantageItem>
+                                <VantageIconBox bgcolor={"purple"}>
+                                    <Coffee weight="fill" />
+                                </VantageIconBox>
+                                <p>O café chega fresquinho até você</p>
+                            </VantageItem>
+                        </VantageItems>
+                    </ContentContainer>
 
-                <ImageContainer>
                     <img src={coffeeMainImg} alt="" />
-                </ImageContainer>
-            </Container>
-        </ContainerImage>
+                </Container>
+            </ContainerImage>
+
+            <CatalogContainer>
+                <h2>Nossos cafés</h2>
+
+                <Card>
+                    <CardHeader>
+                        <img src={coffeeTradicional} alt="" />
+                        <span>TRADICIONAL</span>
+                    </CardHeader>
+                    <CardContent>
+                        <h3>Expresso Tradicional</h3>
+                        <p>O Tradicional café feito com água quente e grãos moídos</p>
+                    </CardContent>
+                    <CardFooter>
+                        <p>R$ <span>9,90</span></p>
+                        <form onSubmit={handleSubmit(handleAddItensCard)}>
+                            <Controller 
+                                control={control}
+                                name="itemAmount"
+                                render={
+                                    ({field}) => {
+                                        return (
+                                            <IncreaseInput>
+                                                <span onClick={() => field.onChange(field.value--)}><Minus size={20} weight="bold" /></span>
+                                                <input type="number" value={field.value} name="itemAmount" />
+                                                <span onClick={() => field.onChange(field.value++)}><Plus size={20} weight="bold" /></span>
+                                            </IncreaseInput>
+                                        )
+                                    }
+                                } 
+                            />
+
+                            
+                            <button type="submit"><ShoppingCart weight="fill" size={20} /></button>
+                        </form>
+                    </CardFooter>
+                </Card>
+
+            </CatalogContainer>
+        </>
+        
     )
 }
--- Trilha ReactJS/2022/08-coffee-delivery/src/pages/Home/styles.ts
+++ Trilha ReactJS/2022/08-coffee-delivery/src/pages/Home/styles.ts
@@ -15,7 +15,7 @@ export const Container = styled.div`
 export const ContentContainer = styled.div`
     width: 60%;
 
-    h2 {
+    h1 {
         font-family: 'Baloo 2';
         font-weight: 800;
         font-size: 3rem;
@@ -65,6 +65,144 @@ export const VantageIconBox = styled.div<VantageIconProps>`
     }
 `
 
-export const ImageContainer = styled.div`
+export const CatalogContainer = styled.div`
+    padding: 1.5rem 25rem;
+
+    h2 {
+        color: ${props => props.theme["base-subtitle"]};
+        margin-bottom: 2rem;
+        font-family: 'Baloo 2';
+        font-size: 2rem;
+    }
+`
+
+export const Card = styled.div`
+    background-color: ${props => props.theme["base-card"]};
+    padding: 1rem;
+    border-top-right-radius: 40px;
+    border-top-left-radius: 10px;
+    border-bottom-left-radius: 40px;
+    border-bottom-right-radius: 10px;
+    width: 16rem;
+
+    display: flex;
+    flex-direction: column;
+`
+
+export const CardHeader = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 1rem;
+
+    img {
+        width: 7.5rem;
+        height: 7.5rem;
+        margin-top: -2rem;
+    }
+
+    span {
+        background-color: ${props => props.theme["yellow-light"]};
+        color: ${props => props.theme["yellow-dark"]};
+        border-radius: 40px;
+        padding: 0.5rem;
+        font-weight: bold;
+        font-size: 0.8rem;
+    }
+`
+
+export const CardContent = styled.div `
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    margin-top: 1rem;
+    margin-bottom: 1rem;
+    gap: 0.5rem;
+
+    h3 {
+        font-family: 'Baloo 2';
+        color: ${props => props.theme["base-subtitle"]};
+        font-size: 1.4rem;
+    }
+
+    p {
+        color: ${props => props.theme["base-label"]};
+        text-indent: 15px;
+    }
+`
+
+export const CardFooter = styled.div`
+    display: flex;
+    justify-content: space-between;
+
+    p {
+        color: ${props => props.theme["base-label"]};
+
+        span {
+            color: ${props => props.theme["base-subtitle"]};
+            font-family: 'Baloo 2';
+            font-weight: bold;
+            font-size: 1.5rem;
+        }
+    }
+
+    form {
+        display: flex;
+        gap: 0.5rem;
+        
+        & > input {
+            width: 3rem;
+        }
+
+        & > button {
+            all: unset;
+            border-radius: 7px;
+            padding: 0.5rem;
+            cursor: pointer;
+            color: ${props => props.theme["white"]};
+            background-color: ${props => props.theme["purple-dark"]};
+            display: flex;
+            justify-content: center;
+            align-items: center;
+
+            svg {
+                background-color: ${props => props.theme["purple-dark"]};
+            }
+        }
+    }
+`
+
+export const IncreaseInput = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: ${props => props.theme["base-button"]};
+    border-radius: 10px;
+    padding: 0.3rem;
+    
+    span {
+        all: unset;
+        color: ${props => props.theme.purple};
+        font-size: 1.5rem;
+
+        svg {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
+    }
+
+    input {
+        all: unset;
+        text-align: center;
+        width: 40px;
+    }
+
+    input[type='number']::-webkit-inner-spin-button, input[type='number']::-webkit-outer-spin-button {
+        -webkit-appearance: none;
+        margin: 0;
+    }
 
 `

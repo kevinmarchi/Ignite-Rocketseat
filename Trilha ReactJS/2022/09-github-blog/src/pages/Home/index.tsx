d196a82c10561f154c952687929fe03abc9c5175
Kevin Marchi <53917192+kevinmarchi@users.noreply.github.com>
2023-04-24 21:34:28 -0300

    09-github-blog - Creating home page

--- Trilha ReactJS/2022/09-github-blog/src/pages/Home/index.tsx
+++ Trilha ReactJS/2022/09-github-blog/src/pages/Home/index.tsx
@@ -1,7 +1,25 @@
+import { InformationContainer, ProfileContainer, ProfileContent, ProfileFooter, ProfileHeader } from "./styles";
+import cameronImg from '../../assets/cameron.png'
+import { Buildings, GithubLogo, Share, Users } from "phosphor-react";
+
 export function Home() {
     return (
-        <>
-            <h1 style={{color: "white"}}>Teste</h1>
-        </>
+        <ProfileContainer>
+            <img src={cameronImg} alt="" />
+            <InformationContainer>
+                <ProfileHeader>
+                    <h2>Cameron Williamson</h2>
+                    <a href="#" target="_blank">GITHUB <Share weight="bold" /></a>
+                </ProfileHeader>
+                <ProfileContent>
+                    <p>Tristique volutpat pulvinar vel massa, pellentesque egestas. Eu viverra massa quam dignissim aenean malesuada suscipit. Nunc, volutpat pulvinar vel mass.</p>
+                </ProfileContent>
+                <ProfileFooter>
+                    <span title="Github"><GithubLogo /> cameronwll</span>
+                    <span title="Company"><Buildings /> Rocketseat</span>
+                    <span title="Followers"><Users /> 32 seguidores</span>
+                </ProfileFooter>
+            </InformationContainer>
+        </ProfileContainer>
     )
 }
--- Trilha ReactJS/2022/09-github-blog/src/pages/Home/styles.ts
+++ Trilha ReactJS/2022/09-github-blog/src/pages/Home/styles.ts
@@ -1,3 +1,62 @@
 import styled from "styled-components";
 
-export const Container = styled.div``
+export const ProfileContainer = styled.div`
+    background-color: ${props => props.theme["base-profile"]};
+    padding: 2rem;
+    margin-top: -6rem;
+
+    display: flex;
+    gap: 2rem;
+`
+export const InformationContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+`
+
+export const ProfileHeader = styled.div`
+    display: flex;
+    justify-content: space-between;
+
+    h2 {
+        color: ${props => props.theme["base-title"]};
+    }
+
+    a {
+        text-decoration: none;
+        color: ${props => props.theme["blue"]};
+        font-weight: bold;
+
+        display: flex;
+        gap: 0.5rem;
+        justify-content: center;
+        align-items: center;
+    }
+`
+
+export const ProfileContent = styled.div`
+    p {
+        color: ${props => props.theme["base-text"]};
+    }
+`
+
+export const ProfileFooter = styled.div`
+    color: ${props => props.theme["base-subtitle"]};
+
+    display: flex;
+    gap: 1rem;
+
+    span {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        gap: 0.2rem;
+
+        svg {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
+    }
+
+`

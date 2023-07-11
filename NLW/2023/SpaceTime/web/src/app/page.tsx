88b9e2a75430deed8379655da47e4509b78c460d
Kevin Marchi <kevin_marchi@hotmail.com>
2023-07-11 16:37:15 -0300

    nlw-spacetime - Initial Setup

--- NLW/2023/SpaceTime/web/src/app/page.tsx
+++ NLW/2023/SpaceTime/web/src/app/page.tsx
@@ -1,10 +1,8 @@
-import { Button } from "@/components/Button";
 
 export default function Home() {
 	return (
-		<>
-			<h1>Hello World</h1>
-			<Button />
-		</>
+		<div className="h-screen bg-zinc-950 text-zinc-50 p-6">
+			<h1 className="text-4xl font-bold">Sua cápsula do tempo</h1>
+		</div>
 	)
 }

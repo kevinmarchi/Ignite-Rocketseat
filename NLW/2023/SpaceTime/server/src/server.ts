548189c84604228c8eddc45d523b4a63d9e98bd8
Kevin Marchi <kevin_marchi@hotmail.com>
2023-07-18 16:52:11 -0300

    nlw-spacetime - GitHub Authentication

--- NLW/2023/SpaceTime/server/src/server.ts
+++ NLW/2023/SpaceTime/server/src/server.ts
@@ -1,13 +1,21 @@
+import 'dotenv/config'
 import fastify from 'fastify'
 import cors from '@fastify/cors'
+import jwt from '@fastify/jwt'
 import { memoriesRoute } from './routes/memories'
+import { authRoutes } from './routes/auth'
 
 const app = fastify()
 
 app.register(cors, {
     origin: true,
 })
+app.register(jwt, {
+    secret: 'spacetime',
+})
+
 app.register(memoriesRoute)
+app.register(authRoutes)
 
 app.listen({
     port: 3333,
---
+++ NLW/2023/SpaceTime/web/src/components/Copyright.tsx
@@ -0,0 +1,17 @@
+export function Copyright() {
+    return (
+        <div 
+            className="text-sm leading-relaxed text-gray-200"
+        >
+            Feito com 💜 no NLW da {' '}
+            <a 
+                href="http://kevinmarchi.dev.br" 
+                target="_blank" 
+                className="underline hover:text-gray-100 transition-colors" 
+                rel="noreferrer"
+            >
+                Rocketseat
+            </a>
+        </div>
+    )
+}

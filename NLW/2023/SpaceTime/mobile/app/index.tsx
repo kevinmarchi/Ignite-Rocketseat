85e57fa383bdb60f2a683205dbc2ba216c963ce2
Kevin Marchi <kevin_marchi@hotmail.com>
2023-07-21 16:38:22 -0300

    nlw-spacetime - Mobile new memories

--- NLW/2023/SpaceTime/mobile/app/index.tsx
+++ NLW/2023/SpaceTime/mobile/app/index.tsx
@@ -1,19 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import { ImageBackground, Text, TouchableOpacity, View } from 'react-native';
+import { Text, TouchableOpacity, View } from 'react-native';
 import * as SecureStore from 'expo-secure-store';
-import {useFonts, Roboto_400Regular, Roboto_700Bold} from '@expo-google-fonts/roboto';
-import {BaiJamjuree_700Bold} from '@expo-google-fonts/bai-jamjuree';
-import blurBg from '../src/assets/bg-blur.png';
-import Stripes from '../src/assets/stripes.svg';
 import NlwLogo from '../src/assets/nlw-spacetime-logo.svg';
-import { styled } from 'nativewind';
 import { makeRedirectUri, useAuthRequest } from 'expo-auth-session';
 import { useEffect } from 'react';
 import { api } from '../src/lib/api';
 import { useRouter } from 'expo-router'
-
-
-const StyledStripes = styled(Stripes)
 
 const discovery = {
   authorizationEndpoint: 'https://github.com/login/oauth/authorize',
@@ -24,12 +15,6 @@ const discovery = {
 export default function App() {
 
   const router = useRouter()
-
-  const [hasLoadedFonts] = useFonts({
-    Roboto_400Regular,
-    Roboto_700Bold,
-    BaiJamjuree_700Bold
-  })
 
   const [request, response, signInWithGithub] = useAuthRequest(
     {
@@ -64,14 +49,8 @@ export default function App() {
     }
   }, [response]);
 
-  if (!hasLoadedFonts) {
-    return null;
-  }
-
   return (
-    <ImageBackground source={blurBg} className='relative flex-1 bg-gray-900 px-8 py-10' imageStyle={{ position: 'absolute', left: '-100%' }}>
-      <StyledStripes className='absolute left-2' />
-
+    <View className='flex-1 px-8 py-10'>
       <View className='flex-1 items-center justify-center gap-6'>
         <NlwLogo></NlwLogo>
         <View className='space-y-2'>
@@ -86,8 +65,6 @@ export default function App() {
       <Text className='text-center font-body text-sm leading-relaxed text-gray-200'>
         Feito com 💜 no NLW da Rocketseat
       </Text>
-
-      <StatusBar style="light" />
-    </ImageBackground>
+    </View>
   );
 }

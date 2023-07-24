e8c6ef373e1c637a1216dfdc433289e0a5ca276f
Kevin Marchi <kevin_marchi@hotmail.com>
2023-07-24 14:58:56 -0300

    nlw-spacetime - Upload route

--- NLW/2023/SpaceTime/server/src/server.ts
+++ NLW/2023/SpaceTime/server/src/server.ts
@@ -2,10 +2,20 @@ import 'dotenv/config'
 import fastify from 'fastify'
 import cors from '@fastify/cors'
 import jwt from '@fastify/jwt'
+import multipart from '@fastify/multipart'
 import { memoriesRoute } from './routes/memories'
 import { authRoutes } from './routes/auth'
+import { uploadRoutes } from './routes/upload'
+import { resolve } from 'node:path'
 
 const app = fastify()
+
+app.register(multipart)
+
+app.register(require('@fastify/static'), {
+    root: resolve(__dirname, '../uploads'),
+    prefix: '/uploads',
+  })
 
 app.register(cors, {
     origin: true,
@@ -16,6 +26,7 @@ app.register(jwt, {
 
 app.register(memoriesRoute)
 app.register(authRoutes)
+app.register(uploadRoutes)
 
 app.listen({
     port: 3333,

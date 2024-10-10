06da865a7a1ef2eac6f9608f1417e56c4cee0879
Kevin Marchi <53917192+kevinmarchi@users.noreply.github.com>
2024-10-10 08:46:26 -0300

    nlw-pocket - Finalizando Backend

--- NLW/2024/Pocket/server/src/http/server.ts
+++ NLW/2024/Pocket/server/src/http/server.ts
@@ -1,53 +1,27 @@
 import fastify from "fastify";
 import { serializerCompiler, validatorCompiler, type ZodTypeProvider } from "fastify-type-provider-zod";
-import { createGoal } from "../functions/create-goal";
-import z from 'zod'
-import { getWeekPendingGoals } from "../functions/get-week-pending-goals";
-import { createGoalCompletion } from "../functions/create-goal-completion";
+import { createGoalRoute } from "./routes/create-goal";
+import { createCompletionRoute } from "./routes/create-completion";
+import { getPendingGoalsRoute } from "./routes/get-pending-goals";
+import { getWeekSummaryRoute } from "./routes/get-week-summary";
+import fastifyCors from "@fastify/cors";
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
+app.register(fastifyCors, {
+    origin: '*',
+})
+
 app.setValidatorCompiler(validatorCompiler);
 app.setSerializerCompiler(serializerCompiler);
+
+app.register(createGoalRoute)
+app.register(createCompletionRoute)
+app.register(getPendingGoalsRoute)
+app.register(getWeekSummaryRoute)
 
 app.listen({
     port: 3333,
 }).then(() => {
     console.log('Http server runing!')
-})
-
-app.post('/goals', {
-    schema: {
-        body: z.object({
-            title: z.string(),
-            desiredWeeklyFrequency: z.number().int().min(1).max(7)
-        })
-    }
-} , async (request) => {
-    
-    const { title, desiredWeeklyFrequency } = request.body
-    await createGoal({
-        title: title,
-        desiredWeeklyFrequency: desiredWeeklyFrequency
-    })
-})
-
-app.get('/pending-goals', async () => {
-    const {pendingGoals} = await getWeekPendingGoals()
-
-    return {pendingGoals}
-})
-
-app.post('/completions', {
-    schema: {
-        body: z.object({
-            goalId: z.string(),
-        })
-    }
-} , async (request) => {
-    
-    const { goalId } = request.body
-    await createGoalCompletion({
-        goalId: goalId,
-    })
 })

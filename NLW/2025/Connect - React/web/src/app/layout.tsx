b98c3acafee3158d242548d1814200680f94ee2e
Kevin Marchi <53917192+kevinmarchi@users.noreply.github.com>
2025-03-05 15:48:15 -0300

    nlw-connect - Aula 02

--- NLW/2025/Connect - React/web/src/app/layout.tsx
+++ NLW/2025/Connect - React/web/src/app/layout.tsx
@@ -19,11 +19,13 @@ const montserrat = Montserrat({
   variable: '--font-montserrat',
 })
 
-export default function RootLayout({children}: Readonly<{children: React.ReactNode;}>) {
+export default function RootLayout({ children }: Readonly<{ children: React.ReactNode; }>) {
   return (
     <html lang="pt-br" className={`${oxanium.variable} ${montserrat.variable}`}>
       <body className='bg-gray-900 text-gray-100 antialiased bg-[url(/background.png)] bg-no-repeat bg-top md:bg-right-top'>
-        {children}
+        <main className="max-w-[1240px] mx-auto px-5 py-8 md:py-0">
+          {children}
+        </main>
       </body>
     </html>
   );
